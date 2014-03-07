91c220cac020fe4d79749fee98a1fbc6f0f02be9
teschnei <teschnei@gmail.com>
2014-03-06 18:43:03 -0700

    Session creation and lookup in packet parser

--- Server/src/server.cpp
+++ Server/src/server.cpp
@@ -5,19 +5,20 @@
 int8*  g_PBuff = NULL;
 int8*  PTempBuff = NULL;
 int32 fd;
+session_list_t session_list;
 
 void server_init()
 {
 	ShowStatus("Initializing server...\n");
 
-	//init packet parse
+	//init packet parser
 	//init sql handle
 
 	//load all static data from db
 
 	//bind socket
-	ShowStatus("Binding to port: %u", 31111);
-	fd = makeBind_udp(INADDR_ANY, 31111);
+	ShowStatus("Binding to port: %u", 54230);
+	fd = makeBind_udp(INADDR_ANY, 54230);
 	ShowMessage("\t - "CL_GREEN"OK"CL_RESET"\n");
 
 	CREATE(g_PBuff, int8, RECV_BUFFER_SIZE + 20);
@@ -25,6 +26,40 @@ void server_init()
 
 	ShowStatus(CL_GREEN"Game server initialized!"CL_RESET"\n");
 	ShowMessage("---------------------------------------\n");
+}
+
+session_data_t* get_session_ipport(uint64 ipp)
+{
+	session_list_t::iterator i = session_list.begin();
+	while (i != session_list.end())
+	{
+		if ((*i).first == ipp)
+			return (*i).second;
+		i++;
+	}
+	return NULL;
+}
+
+session_data_t* create_session(uint32 ip, uint16 port)
+{
+	session_data_t* session_data = new session_data_t;
+	memset(session_data, 0, sizeof session_data_t);
+
+	// might need this later for checking order of packets
+	//CREATE(session_data->server_packet_data, int8, RECV_BUFFER_SIZE + 20);
+
+	session_data->last_update = time(NULL);
+	session_data->addr = ip;
+	session_data->port = port;
+
+	uint64 port64 = port;
+	uint64 ipp = ip;
+	ipp |= port64 << 32;
+	session_list[ipp] = session_data;
+
+	ShowInfo(CL_WHITE"session created for" CL_RESET":" CL_CYAN"%s" CL_RESET":" CL_CYAN"%u"
+		CL_RESET"\n", ip2str(session_data->addr, NULL), session_data->port);
+	return session_data;
 }
 
 int32 process_sockets(fd_set* rfd)
@@ -63,16 +98,16 @@ int32 process_sockets(fd_set* rfd)
 
 			uint64 port = ntohs(from.sin_port);
 			uint64 ipport = ip | port << 32;
-			/*session_data_t* session = getSessionByIPPort(ipport);
+			session_data_t* session = get_session_ipport(ipport);
 
 			if (!session)
 			{
-				session = createSession(ip, port);
+				session = create_session(ip, (uint16)port);
 				if (!session)
 				{
 					return -1;
 				}
-			}*/
+			}
 
 			//parse packets
 
--- Server/src/server.h
+++ Server/src/server.h
@@ -6,18 +6,26 @@
 #include "cbasetypes.h"
 #include "socket.h"
 
+#include <map>
+
 #define RECV_BUFFER_SIZE 1000
 
 struct session_data_t
 {
-	uint32		addr;
-	uint16		port;
+	uint32		addr;		 //client IP address
+	uint16		port;		 //client port
+	time_t		last_update; //for detecting disconnection
 	//account/user object
 };
 
+typedef std::map<uint64, session_data_t*> session_list_t;
+extern session_list_t session_list;
 extern int32 fd;
+extern session_data_t* get_session_ipport(uint64 ipp);
+extern session_data_t* create_session(uint32 ip, uint16 port);
 
 void server_init();
+
 
 
 #endif
